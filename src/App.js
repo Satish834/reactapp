7ce325199ef039675bc8cd34dda649b675264344
Naragani Satish <naragani.10823481@ltimindtree.com>
2025-01-08 11:50:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ImageList from './components/ImageList';
+//import './aws-config'; // Ensure this file is properly set up
+
+const App = () => (
+  <div className="App">
+    <h1>Processed Images</h1>
+    <ImageList />
+  </div>
+);
+
+export default App;
---
+++ src/components/ImageList.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './ImageList.css';
+
+const ImageList = () => {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await axios.get('https://ubt4dlosea.execute-api.us-east-1.amazonaws.com/dep/metadata?tablename=img-metadata&imgid=HamsaDamayanti-2023_Hamsa-beautyful_picture.jpg');
+        console.log('API Response:', response.data);
+        setImages(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="image-list">
+      <h2>Processed Images Metadata</h2>
+      {images.map((image) => (
+        <div className="image-item" key={image.ImgID}>
+          <img src={`https://proce-imgs.s3.amazonaws.com/${image.ImgID}`} alt={image.Description} />
+          <p><strong>ID:</strong> {image.ImgID}</p>
+          <p><strong>Upload Timestamp:</strong> {image.UploadTimestamp}</p>
+          <p><strong>Description:</strong> {image.Description}</p>
+          <p><strong>Artist:</strong> {image.Artist}</p>
+          <p><strong>Copyright:</strong> {image.Copyright}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ImageList;
